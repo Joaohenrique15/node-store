eaf93dc36c6854d64ba0e48e7d7443139a75c6b2
Joaohenrique15 <joao.henrique_15@hotmail.com>
2022-08-27 19:36:45 -0300

    CORS e limitando json

--- src/app.js
+++ src/app.js
@@ -20,8 +20,20 @@ const products = require('./routes/productRouter')
 const customers = require('./routes/customerRouter')
 const orders = require('./routes/orderRouter')
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({
+    limit: '5mb'
+}));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+// Habilita o CORS
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-access-token');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    next();
+});
 
 app.use('/', index);
 app.use('/products', products);
