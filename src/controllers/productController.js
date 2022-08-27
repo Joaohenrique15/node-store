6af86b13ccd2262232a51145a931d29f9a0f940f
Joaohenrique15 <joao.henrique_15@hotmail.com>
2022-08-27 15:20:28 -0300

    Adicionando Repository

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -2,6 +2,8 @@
 
 const mongoose = require("mongoose");
 const Product = mongoose.model('Product');
+const ValidationContract = require('../validators/fluentValidator');
+const repository = require("../repositories/productRepository");
 
 /*
 O método find busca todos os registros da collection no Mongo
@@ -10,9 +12,8 @@ Active: true - É a condição que eu desejo, só quero os produtos que estão c
 */
 
 exports.get = (req, res, next) => {
-    Product.find({
-        active: true
-    }, 'title price slug')
+    repository
+        .get()
         .then(data => {
             res.status(200).send(data)
         }).catch(e => {
@@ -24,7 +25,8 @@ exports.get = (req, res, next) => {
 };
 
 exports.getById = (req, res, next) => {
-    Product.findById(req.params.id, 'title description price active slug tags')
+    repository
+        .get(req.params.id)
         .then(data => {
             res.status(200).send(data)
         }).catch(e => {
@@ -36,10 +38,8 @@ exports.getById = (req, res, next) => {
 };
 
 exports.getBySlug = (req, res, next) => {
-    Product.findOne({
-        slug: req.params.slug,
-        active: true
-    }, 'title description price slug tags')
+    repository
+        .getBySlug(req.params.slug)
         .then(data => {
             res.status(200).send(data)
         }).catch(e => {
@@ -51,10 +51,8 @@ exports.getBySlug = (req, res, next) => {
 };
 
 exports.getByTag = (req, res, next) => {
-    Product.find({
-        tags: req.params.tag,
-        active: true
-    }, 'title description price slug tags')
+    repository
+        .getByTag(req.params.tag)
         .then(data => {
             res.status(200).send(data)
         }).catch(e => {
@@ -66,8 +64,6 @@ exports.getByTag = (req, res, next) => {
 };
 
 exports.post = (req, res, next) => {
-    var product = new Product(req.body);
-
     let contract = new ValidationContract();
     contract.hasMinLen(req.body.title, 3, 'O título deve conter pelo menos 3 caracteres');
     contract.hasMinLen(req.body.slug, 3, 'O título deve conter pelo menos 3 caracteres');
@@ -76,10 +72,11 @@ exports.post = (req, res, next) => {
     // Se os dados forem inválidos
     if (!contract.isValid()) {
         res.status(400).send(contract.errors()).end();
-        return;    
+        return;
     }
 
-    product.save()
+    repository
+        .create(req.body)
         .then(x => {
             res.status(201).send({
                 message: 'Produto cadastrado com sucesso'
@@ -93,27 +90,23 @@ exports.post = (req, res, next) => {
 };
 
 exports.put = (req, res, next) => {
-    Product.findByIdAndUpdate(req.params.id, {
-        $set: {
-            title: req.body.title,
-            description: req.body.description,
-            price: req.body.price,
-            slug: req.body.slug
-        }
-    }).then(data => {
-        res.status(200).send({
-            message: 'Produto atualizado com sucesso'
+    repository
+        .update(req.params.id, req.body)
+        .then(data => {
+            res.status(200).send({
+                message: 'Produto atualizado com sucesso'
+            });
+        }).catch(e => {
+            res.status(400).send({
+                message: 'Falha ao atualizar o produto',
+                data: e
+            })
         });
-    }).catch(e => {
-        res.status(400).send({
-            message: 'Falha ao atualizar o produto',
-            data: e
-        })
-    });
 };
 
 exports.delete = (req, res, next) => {
-    Product.findByIdAndDelete(req.body.id)
+    repository
+        .delete(req.body.id)
         .then(x => {
             res.status(200).send({
                 message: 'Produto removido com sucesso'
