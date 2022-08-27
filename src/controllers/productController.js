81aed864bb4b8a970bc542324f37f2916d9031f5
Joaohenrique15 <joao.henrique_15@hotmail.com>
2022-08-27 13:39:05 -0300

    Finalizando o CRUD

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -12,37 +12,104 @@ Active: true - É a condição que eu desejo, só quero os produtos que estão c
 exports.get = (req, res, next) => {
     Product.find({
         active: true
-    }, 'title price slug').then(data => {
-        res.status(200).send(data)
+    }, 'title price slug')
+        .then(data => {
+            res.status(200).send(data)
+        }).catch(e => {
+            res.status(400).send({
+                message: 'Erro ao listar os produtos',
+                data: e
+            })
+        });
+};
+
+exports.getById = (req, res, next) => {
+    Product.findById(req.params.id, 'title description price active slug tags')
+        .then(data => {
+            res.status(200).send(data)
+        }).catch(e => {
+            res.status(400).send({
+                message: 'Erro ao listar os produtos',
+                data: e
+            })
+        });
+};
+
+exports.getBySlug = (req, res, next) => {
+    Product.findOne({
+        slug: req.params.slug,
+        active: true
+    }, 'title description price slug tags')
+        .then(data => {
+            res.status(200).send(data)
+        }).catch(e => {
+            res.status(400).send({
+                message: 'Erro ao listar os produtos',
+                data: e
+            })
+        });
+};
+
+exports.getByTag = (req, res, next) => {
+    Product.find({
+        tags: req.params.tag,
+        active: true
+    }, 'title description price slug tags')
+        .then(data => {
+            res.status(200).send(data)
+        }).catch(e => {
+            res.status(400).send({
+                message: 'Erro ao listar os produtos',
+                data: e
+            })
+        });
+};
+
+exports.post = (req, res, next) => {
+    var product = new Product(req.body);
+    product.save()
+        .then(x => {
+            res.status(201).send({
+                message: 'Produto cadastrado com sucesso'
+            });
+        }).catch(e => {
+            res.status(400).send({
+                message: 'Falha ao cadastrar o produto',
+                data: e
+            });
+        });
+};
+
+exports.put = (req, res, next) => {
+    Product.findByIdAndUpdate(req.params.id, {
+        $set: {
+            title: req.body.title,
+            description: req.body.description,
+            price: req.body.price,
+            slug: req.body.slug
+        }
+    }).then(data => {
+        res.status(200).send({
+            message: 'Produto atualizado com sucesso'
+        });
     }).catch(e => {
         res.status(400).send({
-            message: 'Erro ao listar os produtos',
+            message: 'Falha ao atualizar o produto',
             data: e
         })
     });
 };
 
-exports.post = (req, res, next) => {
-    var product = new Product(req.body);
-    product.save().then(x => {
-        res.status(201).send({
-            message: 'Produto cadastrado com sucesso'
-        });
-    }).catch(e => {
-        res.status(400).send({
-            message: 'Falha ao cadastrar o produto',
-            data: e
-        });
-    });
-};
-
-exports.put = (req, res, next) => {
-    const id = req.params.id;
-    res.status(201).send({
-        id: id, item: req.body
-    });
-};
-
 exports.delete = (req, res, next) => {
-    res.status(200).send(req.body);
+    Product.findByIdAndDelete(req.body.id)
+        .then(x => {
+            res.status(200).send({
+                message: 'Produto removido com sucesso'
+            });
+        }).catch(e => {
+            res.status(400).send({
+                message: 'Falha ao remover o produto',
+                data: e
+            })
+        });
 };
--- src/routes/productRouter.js
+++ src/routes/productRouter.js
@@ -4,8 +4,11 @@ const router = express.Router();
 const controller = require('../controllers/productController');
 
 router.get('/getAllProducts', controller.get);
+router.get('/getById/:id', controller.getById);
+router.get('/getBySlug/:slug', controller.getBySlug);
+router.get('/getByTag/:tag', controller.getByTag);
 router.post('/new-product', controller.post);
-router.put('/:id', controller.put);
-router.delete('/:id', controller.delete);
+router.put('/update-product/:id', controller.put);
+router.delete('/remove-product', controller.delete);
 
 module.exports = router;
